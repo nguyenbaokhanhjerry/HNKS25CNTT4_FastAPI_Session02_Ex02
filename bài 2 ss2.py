0619d720b15e962bc47608392d20f23f7c888dee
nguyenbaokhanhjerry <khanhbaonguyen76@gmail.com>
2026-06-27 20:32:50 +0700

    Update bài 2 ss2.py

--- bài 2 ss2.py
+++ bài 2 ss2.py
@@ -13,11 +13,11 @@ def get_student():
 # Endpoint hiện tại trong source code là gì?
 # -> /student
 # Vì sao khi gọi GET /students lại bị lỗi 404 Not Found?
-# -> Vì trong source code không khai báo endpoint "/students", mà chỉ có endpoint "/student".
+# -> Vì trong source code không khai báo endpoint "/students", mà chỉ có endpoint "/student"
 # Vì sao tên endpoint /student chưa phù hợp với yêu cầu lấy danh sách sinh viên?
-# -> Vì "/student" mang ý nghĩa lấy một sinh viên, trong khi yêu cầu của khách hàng là lấy toàn bộ danh sách sinh viên.
+# -> Vì "/student" mang ý nghĩa lấy một sinh viên, trong khi yêu cầu của khách hàng là lấy toàn bộ danh sách sinh viên
 # Vì sao dòng return students[0] chưa đúng với yêu cầu nghiệp vụ?
-# -> Vì students[0] chỉ trả về sinh viên đầu tiên trong danh sách, không trả về toàn bộ danh sách sinh viên.
+# -> Vì students[0] chỉ trả về sinh viên đầu tiên trong danh sách, không trả về toàn bộ danh sách sinh viên
 # API đúng theo yêu cầu khách hàng nên có đường dẫn là gì?
 # -> GET /students
 # 2. Sửa lỗi
